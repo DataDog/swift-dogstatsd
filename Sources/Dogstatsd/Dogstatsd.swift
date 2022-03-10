779c8ae0ed0a4f53ec106c08624e8ed27b6cb2db
Brian Floersch <brian.floersch@datadoghq.com>
2022-03-10 11:30:38 -0500

    add headers

--- Sources/Dogstatsd/Dogstatsd.swift
+++ Sources/Dogstatsd/Dogstatsd.swift
@@ -1,3 +1,6 @@
+/// Unless explicitly stated otherwise all files in this repository are licensed under the MIT License.
+/// This product includes software developed at Datadog (https://www.datadoghq.com/)  Copyright 2022 Datadog, Inc.
+
 /// This file contains a complete, generic interface for dogstatsd.
 /// You may implement a `StatsdSender` sender to support other frameworks.
 
